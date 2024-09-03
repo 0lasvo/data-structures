c6310629a1ec5449775316e19e6deec6064a0993
Olasvo <ozvaldo.perez.mirandagmail.com>
2024-09-02 22:48:28 -0600

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, ViewChild} from '@angular/core';
+import {MatMenuTrigger} from "@angular/material/menu";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-structures';
+   isMenuOpen = false;
+
+  @ViewChild(MatMenuTrigger) menuTrigger!: MatMenuTrigger;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    this.isMenuOpen ? this.menuTrigger.openMenu() : this.menuTrigger.closeMenu();
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    const clickedInside = (event.target as HTMLElement).closest('.menu-icon') ||
+      (event.target as HTMLElement).closest('.mat-menu-panel');
+    if (!clickedInside && this.isMenuOpen) {
+      this.toggleMenu();
+    }
+  }
+
+  /*public apuntadores(){
+    this.service.getApuntadores().subscribe(data => this.answer = data);
+  }*/
 }
