678915e8a7c0e35afdaef62dfd4aed58abe430b7
Osvaldo Pérez Miranda <ozvaldo.raam@gmail.com>
2024-09-06 14:13:28 -0600

    init

--- src/app/Components/data.service.ts
+++ src/app/Components/data.service.ts
@@ -1,22 +1,30 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {JSONRecord} from "./JSONRecord";
+import { HttpClient } from "@angular/common/http";
+import { Observable, BehaviorSubject } from "rxjs";
+import { JSONRecord } from "./JSONRecord";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
+  private jsonDataSubject = new BehaviorSubject<JSONRecord[]>([]);
+  jsonData$ = this.jsonDataSubject.asObservable();
 
   constructor(private http: HttpClient) { }
 
-  public getJsonByName(fileName: string): Observable<any> {
-    return this.http.get(`./assets/json/${fileName}.json`);
+  public loadJsonByName(fileName: string): void {
+    this.http.get<JSONRecord[]>(`./assets/json/${fileName}.json`).subscribe(
+      (data: JSONRecord[]) => {
+        this.jsonDataSubject.next(data);
+      },
+      (error) => {
+        console.error(`Error al cargar el archivo ${fileName}.json: `, error);
+      }
+    );
   }
 
-  public getJSON(): Observable<any> {
-    return this.http.get("./assets/json/mydata.json");
+  public getJsonData(): JSONRecord[] {
+    return this.jsonDataSubject.value;
   }
-
 }
